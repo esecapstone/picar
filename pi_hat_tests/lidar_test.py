54befbe427a56cbf454f09e50b4b35109ffefaa3
dorothy026 <43892077+dorothy026@users.noreply.github.com>
2023-02-28 13:22:46 -0600

    Update and rename lidar_test1.py to lidar_test.py

--- pi_hat_tests/lidar_test.py
+++ pi_hat_tests/lidar_test.py
@@ -32,7 +32,7 @@ pygame.display.update()
 
 # Setup the RPLidar
 PORT_NAME = '/dev/ttyUSB0'
-lidar = RPLidar(None, PORT_NAME)
+lidar = RPLidar(None, PORT_NAME,timeout=3)
 
 # used to scale data to fit on the screen
 max_distance = 0

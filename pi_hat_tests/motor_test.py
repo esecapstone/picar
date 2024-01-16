db5a0317032ac6b40c420c56ad785626a43069f6
WashU ESE Capstone Official Repository <107603745+esecapstone@users.noreply.github.com>
2024-01-16 16:11:05 -0600

    Update motor_test.py

--- pi_hat_tests/motor_test.py
+++ pi_hat_tests/motor_test.py
@@ -12,18 +12,18 @@ def Servo_Motor_Initialization():
    return pca
 
 def Motor_Start(pca):
-   x = input("Press and hold EZ button until the RED LED is off.")
+   x = input("Press and hold EZ button. Once the LED turns red, immediately relase the button. After the LED blink red once, press 'ENTER'on keyboard.")
    Motor_Speed(pca, 1)
-   time.sleep(3)
-   Motor_Speed(pca, 0)
+   time.sleep(2)
+   y = input("If the LED just blinked TWICE, then press the 'ENTER'on keyboard.")
    Motor_Speed(pca, -1)
-   time.sleep(3)
-   Motor_Speed(pca, 0)
-   time.sleep(3)
+   time.sleep(2)
+   z = input("Now the LED should be in solid green, indicating the initialization is complete. Press 'ENTER' on keyboard to proceed")
+   
 
 def Motor_Speed(pca,percent):
    #converts a -1 to 1 value to 16-bit duty cycle
-   speed = ((percent) * 3276) + 65535 * 0.15
+   speed = ((percent) * 3277) + 65535 * 0.15
    pca.channels[15].duty_cycle = math.floor(speed)
    print(speed/65535)
 
@@ -31,9 +31,14 @@ def Motor_Speed(pca,percent):
 pca = Servo_Motor_Initialization()
 Motor_Start(pca)
 
-Motor_Speed(pca, 0.2)
-time.sleep(0.5)
-Motor_Speed(pca, 0.16)
+Motor_Speed(pca, 0)   #stop/neutral position
+time.sleep(2)
+Motor_Speed(pca, -0.15)   #reverse
 time.sleep(3)
 Motor_Speed(pca, 0)
+time.sleep(2)
+Motor_Speed(pca, 0.15)   #forward
+time.sleep(3)
+Motor_Speed(pca, 0)    
+time.sleep(2)
 #Footer
